f1c01dc98644a69a85bd8abd5d2aa1fca5cf00b5
Jesus Lares <jesus.elares@gmail.com>
2020-07-01 22:45:12 +0000

    makes example comment class

--- jelares/portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ jelares/portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+// Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,6 +14,8 @@
 
 package com.google.sps.servlets;
 
+import com.google.sps.data.ExampleComments;
+import java.util.List;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,7 +28,35 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Create the example comments object with 3 comments
+    final ExampleComments threeComments = new ExampleComments(List.of(
+      "Comment1", "Comment2", "Comment3"
+      ));
+
+    // Turn the comments data into a JSON string
+    final String jsonComments = commentsToJson(threeComments);
+
     response.setContentType("text/html;");
     response.getWriter().println("<h1>Hello Jesus!</h1>");
+  }
+
+  /** Turns ExampleComments object into JSON */
+  public String commentToJson(ExampleComments comments) {
+    String jsonComments = "{";
+    int numberOfComments = comments.getNumComments();
+
+    // Adds all comments to the jsonObject
+    for (int commentNumber = 0; commentNumber < numberOfComments; commentNumber++) {
+      jsonComments += "\"comment" + commentNumber + "\": ";
+      jsonComments += "\"" + comments.getComment(commentNumber) + "\"";
+
+      if (commentNumber < (numberOfComments - 1)) {
+        jsonComments += ", ";
+      } else {
+        jsonComments += "}";
+      }
+    }
+
+    return jsonComments;
   }
 }
