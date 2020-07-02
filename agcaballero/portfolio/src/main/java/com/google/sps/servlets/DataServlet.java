1dab9b4cc00e2ade22493735ad65a7d42a95d60d
AnneMarie Caballero <agcaballero@google.com>
2020-07-02 15:53:08 +0000

    Make files pretty

--- agcaballero/portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ agcaballero/portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -29,25 +29,24 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-  
   private class Comment {
     private final String name; // stores name of commenter
     private final String email; // stores email of commenter
     private final String comment; // comment itself
-    
+
     // constructs a Comment object
-    public Comment (String name, String email, String comment) {
+    public Comment(String name, String email, String comment) {
       this.name = name;
       this.email = email;
       this.comment = comment;
     }
-    
+
     // returns comment in format: "name (email): comment"
     public String toString() {
       return name + " (" + email + "): " + comment;
     }
   }
-  
+
   // holds all comments that have been submitted in session so far
   private final ArrayList<Comment> comments = new ArrayList<Comment>();
 
@@ -55,10 +54,9 @@ public class DataServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     StringBuilder commentDivs = new StringBuilder();
 
-    // build a String of divs to add to page 
-    for(Comment comment: comments) {
-      commentDivs.append("<div class='comment-div'><p>" + comment 
-                         + "</p></div>");
+    // build a String of divs to add to page
+    for (Comment comment : comments) {
+      commentDivs.append("<div class='comment-div'><p>" + comment + "</p></div>");
     }
 
     // respond with the commentDivs html
@@ -72,10 +70,10 @@ public class DataServlet extends HttpServlet {
     String name = getParameter(request, "name", "");
     String email = getParameter(request, "email", "");
     String comment = getParameter(request, "comment", "");
-    
+
     comments.add(new Comment(name, email, comment));
-    
-    // Redirect to the comments page 
+
+    // Redirect to the comments page
     response.sendRedirect("comments.html");
   }
 
