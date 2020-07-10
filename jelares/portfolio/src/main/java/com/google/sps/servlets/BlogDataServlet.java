2b79eff0731ee19f8378060380e55a2581930324
Jesus Lares <jesus.elares@gmail.com>
2020-07-10 17:50:18 +0000

    adds basic layout for blog post submission

---
+++ jelares/portfolio/src/main/java/com/google/sps/servlets/BlogDataServlet.java
@@ -0,0 +1,80 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
+import com.google.sps.data.PostBatch;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet that manages the comments page */
+@WebServlet("/blog-data")
+public class BlogDataServlet extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Read the query string to get comment limit
+    int commentLimit = Integer.parseInt(request.getParameter("num-comments"));
+
+    // Query to find all comment entities sorted from newest to oldest
+    Query query = new Query("Comment").addSort("timestamp", SortDirection.ASCENDING);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    // adding all comment entities text to a PostBatch instance
+    final PostBatch comments = new PostBatch(new ArrayList<String>());
+    for (Entity entity : results.asIterable(FetchOptions.Builder.withLimit(commentLimit))) {
+      String commentText = (String) entity.getProperty("comment-text");
+      comments.addPost(commentText);
+    }
+
+    response.setContentType("application/json;");
+    Gson gson = new Gson();
+    String jsonComments = gson.toJson(comments.getPostArray());
+    response.getWriter().println("{\"comments\":" + jsonComments + "}");
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Get the input from the form.
+    final String blogPostTitle = request.getParameter("blog-title");
+    final String blogPostContent = request.getParameter("blog-content");
+    final long timestamp = System.currentTimeMillis();
+
+    // Add the input to datastore
+    Entity commentEntity = new Entity("blog-post");
+    commentEntity.setProperty("blog-post-title", blogPostTitle);
+    commentEntity.setProperty("blog-post-content", blogPostContent);
+    commentEntity.setProperty("timestamp", blogPostContent);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(commentEntity);
+
+    // Redirect back to the HTML page.
+    response.sendRedirect("/comments/dataPage.html");
+  }
+}
