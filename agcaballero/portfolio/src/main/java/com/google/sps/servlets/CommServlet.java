71e7333d1aaa22ef958c288d57831efaee46cfe4
AnneMarie Caballero <agcaballero@google.com>
2020-07-13 18:52:28 +0000

    Add capacity for 'Contact Me' form to post the message to datastore

--- agcaballero/portfolio/src/main/java/com/google/sps/servlets/CommServlet.java
+++ agcaballero/portfolio/src/main/java/com/google/sps/servlets/CommServlet.java
@@ -14,13 +14,12 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
 import java.io.IOException;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -28,50 +27,61 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
 import java.util.Properties;
 
-/** Servlet that allows users to send me emails */
+/** 
+ * Servlet to post and retrieve messages sent
+ * through the "Contact Me" form
+ */
 @WebServlet("/comm")
 public class CommServlet extends HttpServlet {
+  
+  /**
+   * A private class to represent a message
+   * sent from "Contact Me"
+   */
+  private class Message {
+    
+    /** Name of the contacter */
+    private String name;
+    /** Email of the contacter */
+    private String email;
+    /** Subject of the contact message */
+    private String subject;
+    /** Text/main description of the contact message */
+    private String body;
+    
+    public Message(String name, String email, String subject, String body) {
+      this.name = name;
+      this.email = email;
+      this.subject = subject;
+      this.body = body;
+    }
 
-  private static final String FROM = "site-notif@google.com";
-  private static final String RECIPIENT = "agcaballero@google.com"; // my work email
+    public Entity getEntity() {
+      Entity entity = new Entity("Message");
+      entity.setProperty("name", name);
+      entity.setProperty("email", email);
+      entity.setProperty("subject", subject);
+      entity.setProperty("body", body);
+
+      return entity;
+    }
+  }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Retrieve info from form
     String fname = getParameter(request, "fname", "");
     String lname = getParameter(request, "lname", "");
     String email = getParameter(request, "email", "");
     String subject = getParameter(request, "subject", "");
     String body = getParameter(request, "body", "");
-
-    // set up SMTP mail server
-    Properties prop = new Properties();
-    prop.setProperty("mail.smtp.host", "localhost");
-    prop.put("mail.smtp.host", "smtp server ");
-    prop.put("mail.smtp.auth", "true");
-    prop.put("mail.smtp.port", "25");
-    Session session = Session.getDefaultInstance(prop, null);
-
-    System.out.println("ATTN 1");
     
-    try {
-      Message message = new MimeMessage(session);
-
-      // set sender & recipient emails
-      message.setFrom(new InternetAddress(FROM));
-      message.addRecipient(Message.RecipientType.TO, new InternetAddress(RECIPIENT));
-      
-      // set subject, content & date of email
-      message.setSubject(subject);
-      message.setText(body + "\n-" + fname + " " + lname + " (" + email + ")");
-      message.setSentDate(new Date());
-
-      System.out.println("ATTN 2");
-      
-      Transport.send(message);
-      System.out.println("Message sent!");
-    } catch (MessagingException e) {
-      e.printStackTrace();
-    }
+    // create a Message with info from form
+    Message message = new Message(fname + " " + lname, email, subject, body);
+    
+    // add this Message to the database
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(message.getEntity());
 
     response.sendRedirect("/contact.html");
   }
