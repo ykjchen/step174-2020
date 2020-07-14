e94fe5fab62306f4d098fee37d39ea3e04fa2788
AnneMarie Caballero <agcaballero@google.com>
2020-07-14 15:03:55 +0000

    Add form validation using HTML attributes and remove JS form validation

--- agcaballero/portfolio/src/main/webapp/messages.js
+++ agcaballero/portfolio/src/main/webapp/messages.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/* JS Functions for Messages Page
+/* JS Functions for the Messages Page
  * Features: message display, message deletion
  */
 
