eab66a5c050a14bde6343e3e8b29eb83e6a4a6c8
AnneMarie Caballero <agcaballero@google.com>
2020-07-14 21:39:33 +0000

    Write brute force calendar algorithm

--- agcaballero/walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ agcaballero/walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,74 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
 public final class FindMeetingQuery {
+   
+  private static final int MINUTES_IN_DAY = 24 * 60;
+   
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Collection<String> attendees = request.getAttendees();
+    ArrayList<TimeRange> times = new ArrayList<TimeRange>();
+    boolean[] minutes = new boolean[MINUTES_IN_DAY];
+
+    for(int i = 0; i < minutes.length; i++) {
+      minutes[i] = true;
+    }
+
+    for(Event event: events) {
+      // if there's an overlap in attendees block off those times
+      if(attendeeOverlap(event.getAttendees(), attendees)) {
+        TimeRange range = event.getWhen();
+
+        for(int i = range.start(); i < range.end(); i++)
+          minutes[i] = false;
+      }
+    }
+
+    int beginning = 0; 
+    boolean available = minutes[beginning];
+
+    // add available times to times array
+    for(int i = 0; i < minutes.length; i++) {
+      // if all minutes since beginning are true 
+      if(available) {
+        // if current minute is false, add a new time range
+        if(! minutes[i]) {
+          int end = i - 1;
+          int duration = end - beginning;
+
+          if(duration >= request.getDuration())
+            times.add(TimeRange.fromStartDuration(beginning, duration));
+
+          available = false;
+        }
+            
+      }
+      // if current time has been taken until now
+      else {
+        // if now available, set beginning to now & available to true
+        if(minutes[i]) {
+          beginning = i;
+          available = true;
+        }
+      }
+    }
+    
+    return times;
   }
+  
+  /**
+   * @return true if overlap between attendees of two events, false otherwise
+   */
+  private boolean attendeeOverlap(Collection<String> groupA, Collection<String> groupB) {
+    for(String attendeeA: groupA)
+      for(String attendeeB: groupB)
+        if(attendeeA.equals(attendeeB)) return true;
+
+    return false;
+  }
+
 }
