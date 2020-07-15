30c296135fbb918d7c47ced1e4d4b8b2c9bbce3f
AnneMarie Caballero <agcaballero@google.com>
2020-07-15 13:31:25 +0000

    Update year on copyright (& make files pretty)

--- agcaballero/portfolio/src/main/webapp/comments.js
+++ agcaballero/portfolio/src/main/webapp/comments.js
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+// Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 /* JS Functions for Comments Page
- * Featuers: comment display, comment deletion
+ * Features: comment display, comment deletion
  */
 
 // COMMENT DISPLAY
