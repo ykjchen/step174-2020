90ea0bb5f489a4a2d53895f9f5a7e35d307cdb49
Jesus Lares <jesus.elares@gmail.com>
2020-07-16 16:25:09 +0000

    implements FindMeetingQuery passes all tests

--- jelares/walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ jelares/walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+// Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -30,13 +30,20 @@ public final class FindMeetingQuery {
 
     // Get the TimeRanges of the relevant events in a List, ordered by start time.
     List<TimeRange> timeRanges = getEventTimeRanges(relevantEvents); 
-    System.out.println(timeRanges);
 
     // Get a condensed TimeRanges list for the relevent events (condensed means overlapping events are turned into a single event).
     List<TimeRange> condensedTimeRanges = getCondensedTimeRanges(timeRanges);
-    System.out.println(condensedTimeRanges + "\n");
 
-    return condensedTimeRanges;
+    // Get the inverse time ranges in a day, given a collection of non-overlapping time ranges
+    // sorted by their start time. Inverse means all the time ranges not covered by any time range
+    // in the collection.
+    List<TimeRange> openTimeRanges = getInverseTimeRanges(condensedTimeRanges);
+
+    // Filter the open time ranges such that only open time ranges with a duration greater than the duration of the
+    // meeting remain
+    List<TimeRange> validTimeRanges = getValidTimeRanges(openTimeRanges, (int) request.getDuration());
+
+    return validTimeRanges;
   }
 
   // Adds all relevant events (events with at least one required attendee) to a new collection.
@@ -68,6 +75,7 @@ public final class FindMeetingQuery {
   }
 
   /**
+   * Condenses an ordered list of time ranges (ordered by start time).
    * @param timeRanges list of timeRanges sorted by start time
    * @return list of condensed timeRanges (condensed means overlapping events are turned into a single event).
    */
@@ -102,4 +110,54 @@ public final class FindMeetingQuery {
 
     return condensedTimeRanges;
   }
+
+  /**
+   * Finds the timeRanges in the day which are not covered by any events. May add ranges of duration 0
+   * between timeRanges.
+   * @param coveredRanges List of timeRanges which we are finding the inverse of
+   * @return List of timeRanges between the timeRanges in coveredRanges
+   */
+  private List<TimeRange> getInverseTimeRanges(List<TimeRange> coveredRanges) {
+    List<TimeRange> inverseRanges = new ArrayList<>();
+
+    if (coveredRanges.isEmpty()){
+      inverseRanges.add(TimeRange.WHOLE_DAY);
+      return inverseRanges;
+    }
+
+    TimeRange fromStart = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, coveredRanges.get(0).start(), false);
+    inverseRanges.add(fromStart);
+
+    int lastEnd = coveredRanges.get(0).end();
+    int nextStart;
+
+    // With every loop iteration, add the inverse range from [coveredRanges.get(i).end to coveredRanges.get(i+1).start()]
+    // Loop from the second element to the last element: the first and last timeRanges are a special
+    // case as we must look at the inverse TimeRange [0 to first.start()] (1), and [last.end() to end of day] (2).
+    // Ex: [Start of day|--(1)--|First timeRange ... other ranges ... Last timeRange|--(2)--|End of day] 
+    for (int i = 1; i < coveredRanges.size(); i++){
+      nextStart = coveredRanges.get(i).start();
+      TimeRange betweenLastAndNext = TimeRange.fromStartEnd(lastEnd, nextStart, false);
+      inverseRanges.add(betweenLastAndNext);
+      lastEnd = coveredRanges.get(i).end();
+    }
+
+    TimeRange toEnd = TimeRange.fromStartEnd(lastEnd, TimeRange.END_OF_DAY , true);
+    inverseRanges.add(toEnd);
+
+    return inverseRanges;
+   }
+
+  // Filters out all timeRanges with a duration less than the duration of the meeting.
+   private List<TimeRange> getValidTimeRanges(List<TimeRange> timeRanges, int duration) {
+     List<TimeRange> validTimeRanges = new ArrayList<>();
+
+     for (TimeRange timeRange : timeRanges) {
+       if (timeRange.duration() >= duration) {
+         validTimeRanges.add(timeRange);
+       }
+     }
+
+     return validTimeRanges;
+   }
 }
